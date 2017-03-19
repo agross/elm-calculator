763e0b43380e88d4a8a1b10db356ae2b3b8c9636
Alexander Groß <agross@therightstuff.de>
2017-03-19 11:33:18 +0100

    display and one button

--- main.elm
+++ main.elm
@@ -1,13 +1,43 @@
 module Calc exposing (..)
+
 -- exposes everything
 
 import Html
 
+
+-- shorten module name
+
+import Html.Attributes as Attr
+
+
 -- define type for `main` value
+
+
 main : Html.Html msg
 main =
-  display
+    Html.div []
+        [ display 0
+        , button "1"
+        ]
 
-display : Html.Html msg
-display =
-  Html.h1 [] [ Html.text "0" ]
+
+display : Int -> Html.Html msg
+display number =
+    Html.h1
+        [ Attr.style
+            [ ( "background", "lightgray" )
+            , ( "text-align", "right" )
+            ]
+        ]
+        [ Html.text (toString number) ]
+
+
+button : String -> Html.Html msg
+button text =
+    Html.button
+        [ Attr.style
+            [ ( "width", "30px" )
+            , ( "height", "30px" )
+            ]
+        ]
+        [ Html.text text ]
